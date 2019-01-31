4afc1da6a17556bbb8ab9a8ce5ae55aa0ddbc041
Bromoo <carnage4cod@gmail.com>
2019-01-31 17:33:41 +0100

    Update Get-LastLogon.ps1

--- Get-LastLogon.ps1
+++ Get-LastLogon.ps1
@@ -9,81 +9,73 @@ function _Get-ADUserLastLogon
 
         [Parameter(Mandatory=$true)] 
         [ValidateNotNullOrEmpty()] 
-        [Object]$DCs
+        [Object]$Domain
     )
 
-    # Default Vars
-    $Time = 0
-    $Date = $null
 
-    # Query all DCs for lastlogonstimestamp and return the most recent
-    foreach ($DC in $DCs)
-    { 
-        $LastLogon = Get-ADUser $User -Server $DC -Properties LastLogonTimestamp
-        If ($LastLogon.LastLogontimestamp -gt $Time)
-        {
-            $Time        = $LastLogon.LastLogontimestamp
-            $LastLogonDC = $DC.Hostname
-        }
-    }
-
-    # If date can not be resolved set date to unknown
-    If ($Date -eq "01-01-1601 01:00:00")
-    {
-        $Date = "Unknown"
-    }
-    Else
-    {
-        $Date = [DateTime]::FromFileTime($Time).ToString('dd-MM-yyyy HH:mm:ss')
-    }
-
-    return $Date, $LastLogonDC
-}
-
-
-$Domain = 'Domain'
-$User   = 'SamAccountName'
-
-# Validate domain
-Try
-{
-    Get-ADDomain -Identity $Domain -ErrorAction Stop | Out-Null
-    $DomainValid = $true
-
-    # Validate User
+    # Validate domain
     Try
     {
-        $User = Get-ADUser -Identity $User -Server $Domain -ErrorAction Stop
+        # Validate Domainname
+        Get-ADDomain -Identity $Domain -ErrorAction Stop | Out-Null
+        
+        # Validate Domaincontrollers
+        $DCs = Get-ADDomainController -Filter * -Server $Domain
+        If ($DCs)
+        {
+            # Validate User
+            Try
+            {
+                $User = Get-ADUser -Identity $User -Server $Domain -ErrorAction Stop
+            }
+            Catch
+            {
+                Throw $_
+            }
+        }
+        Else
+        {
+            Throw "Unable to resolve domaincontrollers from [$Domain]"
+        }
     }
     Catch
     {
         Throw $_
     }
-}
-Catch
-{
-    Throw $_
-}
 
-If ($User)
-{
-    # Retrieve all domaincontrollers from specified domain.
-    $DCs    = Get-ADDomainController -Filter * -Server $Domain
-    If ($DCs)
+    If ($User)
     {
-        # Retrieve most recent logon
-        Try
-        {
-            $GetLastLogon = _Get-ADUserLastLogon -User $User.SamAccountName -DCs $DCs
-            Write-Host "LastLogon for user [$($User.SamAccountName)]: $($GetLastLogon[0]) from DC [$($GetLastLogon[1])]"
+        # Default Vars
+        $Time = 0
+        $Date = $null
+
+        # Query all DCs for lastlogonstimestamp and return the most recent
+        foreach ($DC in $DCs)
+        { 
+            $LastLogon = Get-ADUser $User -Server $DC -Properties LastLogonTimestamp
+            If ($LastLogon.LastLogontimestamp -gt $Time)
+            {
+                $Time        = $LastLogon.LastLogontimestamp
+                $LastLogonDC = $DC.Hostname
+            }
         }
-        Catch
+
+        # If date can not be resolved set date to unknown
+        If ($Date -eq "01-01-1601 01:00:00")
         {
-            Throw $_
+            $Date = "Unknown"
         }
+        Else
+        {
+            $Date = [DateTime]::FromFileTime($Time).ToString('dd-MM-yyyy HH:mm:ss')
+        }
+        
+            return $Date, $LastLogonDC
     }
     Else
     {
-        Throw "No Domaincontrollers could be resolved from domain [$($Domain)]"
+        return $false
     }
 }
+
+_Get-ADUserLastLogon -User SamAccountName -Domain Domain.com
